c6c965f05ef50fdc3dc19e72ef1fd44fc5854fed
must <must@gmail.com>
2023-07-10 15:11:53 +0400

    no changes,useless commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include "./includes/Server.hpp"
 
+static int	port_parse(char const *av)
+{
+	return strncmp(av, "6667", 5) == 0;
+}
+
 int main(int argc, char const *argv[])
 {
 	if (argc != 2) {
 		std::cerr << "Usage: ./ircserv [port]" << std::endl;
 		return 1;
 	}
-	(void)argv; // mustafa, attack ????
-	
+	if(!port_parse(argv[1]))
+	{
+		std::cerr << "wrong port\n";
+		return 1;
+	}
 	Server server;
 	return 0;
 }
