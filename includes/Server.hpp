313a24ee9c74824c5f1105a7deb1ce80c9c3b979
must <mradwan@student.42abudhabi.ae>
2023-07-10 20:52:56 +0400

    parse

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -1,12 +1,19 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <vector>
+#include <algorithm>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <sys/select.h>
 #include <fcntl.h>
 #include <cerrno>
+#include <iostream>
+#include <sstream>
 
 const int MAX_CLIENTS = 10;
 const int BUFFER_SIZE = 1024;
@@ -25,6 +32,8 @@ private:
 	struct sockaddr_in address;
 	char buffer[BUFFER_SIZE];
 	fd_set readfds;
+
+	std::vector<std::string> _cmd;
 
 public:
 	Server(void);
@@ -47,6 +56,9 @@ public:
 	int *getClientSockets(void);
 	fd_set getReadfds(void);
 	struct sockaddr_in getAddress(void);
-
+	int	parse_cmds(std::string str);
 };
 
+std::vector<std::string> split(std::string str);
+
+#endif
