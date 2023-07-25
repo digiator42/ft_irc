72aa5b68287cc270f2a507006bf37e64330b05d2
must <mradwan@student.42abudhabi.ae>
2023-07-25 19:31:47 +0400

    fixed

--- Utils.cpp
+++ Utils.cpp
@@ -1,184 +1,109 @@
-#include "./includes/Server.hpp"
-#include "./includes/Command.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include "includes/Server.hpp"
+#include "includes/Command.hpp"
 
-User::User(int fd, int id) : _fd(fd), _id(id), isAuth(false), isOperator(false), nickName(""), userName(""), _cmd(2)
-{
-	is_registered = false;
-	change_flag = false;
-	pass_issue = 0;
-	alr_reg = 0;
-	std::cout << "User created" << std::endl;
-	input = "";
+User &Utils::find(int fd) {
+	for(std::vector<User>::iterator it = Server::_users.begin(); it != Server::_users.end(); ++it) {
+        if (it->_fd == fd) {
+            std::cout << "Utils::find:: u=User FD : " << it->_fd << std::endl;
+			return *it;
+        }
+    }
+	throw Server::ServerException("Utils::find: User not found");
 }
 
-User::~User() {}
+void Utils::signalHandler(int signum) {
 
-void User::userErase(User &user)
-{
-	for(std::vector<User>::iterator it = Server::_users.begin(); it != Server::_users.end(); ++it)
-	{
-		if (it->_fd == user._fd) {
-			Server::_users.erase(it);
-			--it;
-		}
-	}
+    std::cout << RED << "Interrupt signal (" << signum << ") received." << RESET << "\n";
+
+    for(std::vector<int>::iterator it = Server::_fds.begin(); it != Server::_fds.end(); ++it) {
+        if (*it != 0) {
+            std::cout << "Closing socket: " << *it << std::endl;
+            close(*it);
+        }
+    }
+    close(Server::serverSocket);
+    exit(signum);
 }
 
-void User::whoAmI(User &user)
+void Utils::closeThis(User &user)
 {
-	std::cout << CYAN << user << RESET <<std::endl;
-	std::string userDetails = "UserName: [" + user.userName + "]" + ", Nick: " + "[" + user.nickName + "]" + ", Auth: "
-			+ "[" + (user.isAuth ? "YES" : "NO") + "]" + ", [fd: " + Utils::to_string(user._fd) +  "]" + ".\n";
-	send(user._fd, (YELLOW + userDetails + RESET).c_str(), userDetails.length() + strlen(YELLOW) + strlen(RESET) , 0);
-
-}
-
-void User::showClients(User &user)
-{
-	(void)user;
-	for (int i = 0; i < Server::max_sd; i++)
-	std::cout << "client " << Server::_fds.at(i) << " " << std::endl;
-}
-
-void User::showUsers(User &user)
-{
-	(void)user;
+	std::cout << YELLOW << user.input << RESET;
+	std::cout << RED << "User " << user._fd << " closed" << RESET << std::endl;
+	close(user._fd);
+	
+    Server::_fds.erase(std::find(Server::_fds.begin(), Server::_fds.end(), user._fd));	
+    Server::_users.erase(std::find(Server::_users.begin(), Server::_users.end(), user));
 	Server::showUsers();
 }
 
-int User::authorise(User *user, std::string cmd)
-{
-	if(user->isAuth == true)
-		return 2;
-	if (parse_cmd(cmd))
-	{
-		if(!is_registered)
-		{
-			for(std::vector<User>::iterator it = Server::_users.begin(); it != Server::_users.end(); ++it)
-			{
-				if(_cmd[0] != "" && _cmd[1] != "" && pass != ""){
-					if (_cmd.size() > 1 && it->is_registered  && (it->nickName == _cmd[1]|| it->userName == _cmd[0]))
-					{
-						std::string S = ERR_ALREADYREGISTRED;
-						S.append(" User already registered\n");
-						send(user->_fd, S.c_str(), strlen(S.c_str()), 0);
-						alr_reg = 1;
-						Utils::closeThis(*user);
-						return 0;
-					}
-				}
-			}
-		}
-		std::cout << "nick : " << nickName << "\n";
-		user->nickName = _cmd[1];
-		user->userName = _cmd[0];
 
-		if(user->nickName != "" && user->userName != "" && pass == Server::getPassword() && !is_registered)
-		{
-			const char *msg = ":irc 001 user :Welcome hello\n"
-				":irc 002 user :Host are none\n"
-				":irc 003 user :Created\n";
-			send(user->_fd, msg, strlen(msg), 0);
-			if(pass == Server::getPassword()){
-				user->isAuth = true;
-				is_registered = true;
-			}
-		}
-
-		if(pass != "")
-		{
-			if(this->pass != Server::getPassword())
-				{
-					std::string S = WRONG_PASS_CODE;
-					S.append(" : Wrong password");
-					send(this->_fd, S.c_str(), strlen(S.c_str()), 0);
-					pass_issue = 1;
-					return false;
-				}
-		}
-		if(isAuth)
-			send(user->_fd, "\033[32mAUTHENTICATED\n\033[0m", strlen("\033[32mAUTHENTICATED\n\033[0m"), 0);
-		change_flag	= true;
-		return 1;
-	}
-	return 0;
+std::string Utils::to_string(int value) {
+    std::stringstream ss;
+	ss << value;
+	std::string s = ss.str();
+	return s;
+}
+std::vector<std::string> Utils::split(const std::string str) {
+	std::vector<std::string> vector;
+	std::istringstream iss(str);
+	std::string cmd;
+	while (iss >> std::skipws >> cmd)
+		vector.push_back(cmd);
+	return vector;
+}
+std::string Utils::trim(std::string &str) {
+    size_t left = str.find_first_not_of('\n');
+    size_t right = str.find_last_not_of('\n');
+    if (left == std::string::npos || right == std::string::npos)
+        return "";
+    return str.substr(left, right - left + 1);
 }
 
-void	User::user_options(User *user, std::vector<std::string> splitmsg)
-{
-	if (splitmsg.size() > 0 && (splitmsg.at(0) == "quit" || splitmsg.at(0) == "exit" || splitmsg.at(0) == "close"))
-		Utils::closeThis(*user);
-	else if (splitmsg.size() > 0 && splitmsg.at(0) == "help")
-	{
-		std::string help = "Available commands: \n"
-		"whoami - show user details\n"
-		"show clients - show all clients\n"
-		"show users - show all users\n"
-		"kick <nick> - kick user\n"
-		"exit - close connection\n"
-		"quit - close connection\n"
-		"close - close connection\n"
-		"help - show help\n";
-		send(user->_fd, help.c_str(), help.length(), 0);
-	}
+void sendErrorMessage(int fd, const std::string& message, const std::string& key) {
+    std::string errorMsg = key + " ERROR: " + message;
+    send(fd, errorMsg.c_str(), strlen(errorMsg.c_str()), 0);
 }
 
-void User::user_cmds(User* user, std::vector<std::string> splitmsg) {
-    if (splitmsg.empty()) {
-        return;
+void handleJoinCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user) {
+    if (splitmsg.size() == 2 || splitmsg.size() == 3) {
+        cmd.join(splitmsg.at(1), "", *user);
+    } else {
+		return ; // temporary
+        // sendErrorMessage(user->_fd, "JOIN command requires 2 or 3 arguments\n", TOO_MANY_ARGS);
     }
+}
 
-    Command cmd;
-    std::string cmdType = splitmsg.at(0);
-	std::cout << "cmdType -> {" << cmdType << "}" << std::endl;
-    if (cmdType == JOIN) {
-        handleJoinCommand(splitmsg, cmd, user);
-    } else if (cmdType == KICK) {
-        handleKickCommand(splitmsg, cmd, user);
-    } else if ((splitmsg.size() == 2 || splitmsg.size() == 3) && splitmsg.at(0) == "TOPIC") { // not yet
-        cmd.topic(splitmsg.at(1), splitmsg.at(2), *user);
-    } else if (cmdType == PRIVMSG) {
-        handlePrivMsgCommand(splitmsg, cmd, user);
-    } else if (cmdType == INVITE) {
-        handleInviteCommand(splitmsg, cmd, user);
-    } else if (cmdType == PING) {
-		send(user->_fd, "PONG\n", strlen("PONG\n"), 0);
-	} else if (cmdType == WHOIS) {
-		handleWhoisCommand(splitmsg, cmd, user);
-	}else if (cmdType == MODE) {
-		handleModeCommand(splitmsg, cmd, user);
+void handleKickCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user) {
+    if (splitmsg.size() == 4) {
+        cmd.kick(splitmsg.at(1), splitmsg.at(2), splitmsg.at(3), *user);
+    } else if (splitmsg.size() == 3) {
+        cmd.kick(splitmsg.at(1), splitmsg.at(2), "", *user);
+    } else {
+        sendErrorMessage(user->_fd, "KICK command requires 3 or 4 arguments\n", TOO_MANY_ARGS);
+    }
+}
+
+void handlePrivMsgCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user) {
+	if (splitmsg.size() == 3) {
+		cmd.privmsg(splitmsg.at(1), splitmsg.at(2), *user); // second argument will be the split message for mutiple words
+	} else if (splitmsg.size() == 2) {
+		sendErrorMessage(user->_fd, "PRIVMSG command requires atleast 3 arguments\n", PRIVMSG_EMPTY);
+	} else if(splitmsg.size() == 1) {
+		sendErrorMessage(user->_fd, "PRIVMSG command requires atleast 3 arguments\n", ERR_NOSUCHNICK);
+	} else {
+		sendErrorMessage(user->_fd, "PRIVMSG command requires atleast 3 arguments\n", TOO_MANY_ARGS);
 	}
+}
 
-	int i = 1;
-	int j;
-	for (std::vector<Channel>::iterator it = Server::_channels.begin(); it != Server::_channels.end(); it++)
-	{
-		std::cout << "Channel " << i << "'s name in server vector -> " << it->getName() << std::endl;
-		std::vector<User> temp_users = it->getUsers();
-		j = 1;
-		for (std::vector<User>::iterator it_u = temp_users.begin(); it_u != temp_users.end(); it_u++)
-		{
-			std::cout << "User " << j << " - " << it_u->nickName << std::endl;
-			j++;
-		}
-		j = 1;
-		for (std::vector<User>::iterator it_i = it->invites.begin(); it_i != it->invites.end(); it_i++)
-		{
-			std::cout << "Invite " << j << " - " << it_i->nickName << std::endl;
-			j++;
-		}
-		i++;
-		std::cout << "Channel's modes -> " << std::endl;
-		if (it->isMode('i') == 1)
-			std::cout << "mode +i" << std::endl;
-		if (it->isMode('o') == 1)
-			std::cout << "mode +o" << std::endl;
-		if (it->isMode('l') == 1)
-			std::cout << "mode +l" << std::endl;
-		if (it->isMode('t') == 1)
-			std::cout << "mode +t" << std::endl;
-		if (it->isMode('k') == 1)
-			std::cout << "mode +k" << std::endl;
+void	handleInviteCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user)
+{
+	if (splitmsg.size() == 3) {
+		cmd.invite(splitmsg.at(1), splitmsg.at(2), *user);
+	} else {
+		sendErrorMessage(user->_fd, "INVITE command requires 3 arguments\n", TOO_MANY_ARGS);
 	}
 }
 
@@ -203,104 +128,16 @@ void handleWhoisCommand(const std::vector<std::string>& splitmsg, Command& cmd, 
 	}
 }
 
-void User::execute(std::string cmd, User *user)
+void handleModeCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user)
 {
-	std::string levels[3] = {"whoami", "show clients", "show users"};
-	void (User::*f[3])(User &user) = { &User::whoAmI, &User::showClients, &User::showUsers};
-	std::vector<std::string> splitmsg = Utils::split(cmd);
-
-	if(!authorise(user, cmd))
-	{
-		if(splitmsg.size() > 0 && splitmsg.at(0) != "CAP"){
-			if(pass_issue != 1 && alr_reg != 1)
-			{
-				std::string S = ERR_NOTREGISTERED;
-				S.append(" You have not registered\n");
-				std::cout << "->>>>>>>>>" << splitmsg.size() << std::endl;
-				send(user->_fd, S.c_str(), strlen(S.c_str()), 0);
-			}
-		}
+	if (splitmsg.size() == 3){
+		cmd.mode(splitmsg.at(1), splitmsg.at(2), *user, "");
 	}
-	std::cout << "hiiiiiiii->>>>>>|||" << cmd << "|" << std::endl;
-
-	if(splitmsg.size() > 0 && splitmsg.at(0) == "NICK" && change_flag == false)
-		change_user(user, splitmsg);
-
-	if ((splitmsg.size() > 1 && splitmsg.at(0) == "CAP"))
-	{
-		if(splitmsg.size() >= 3 && splitmsg.at(1) == "LS" && splitmsg.at(2) == "302")
-		{
-			std::string S = "CAP * ACK :multi-prefix\r\n";
-			send(user->_fd, S.c_str(), strlen(S.c_str()), 0);
-		}
-		else if (splitmsg.size() >= 2 && splitmsg.at(1) == "LS")
-		{
-			std::string S = "CAP * ACK :multi-prefix\r\n";
-			send(user->_fd, S.c_str(), strlen(S.c_str()), 0);
-		}
-		else if (splitmsg.size() >= 3 && splitmsg.at(1) == "REQ" && splitmsg.at(2) == "multi-prefix")
-		{
-			std::string S = "CAP * ACK :multi-prefix\n";
-			send(user->_fd, S.c_str(), strlen(S.c_str()), 0);
-		}
+	else if(splitmsg.size() == 4){
+		cmd.mode(splitmsg.at(1), splitmsg.at(2), *user, splitmsg.at(3));
 	}
-
-	if(this->isAuth)
-	{
-		user_options(user, splitmsg);
-		user_cmds(user, splitmsg);
-		change_flag = false;
+	else {
+		sendErrorMessage(user->_fd, "MODE command requires 2 or 3 arguments\n", TOO_MANY_ARGS);
 	}
-		for (int i = 0; i < 3; i++)
-		{
-			cmd = Utils::trim(cmd);
-			cmd == levels[i] ? (this->*f[i])(*user) : (void)0;
-		}
-
-	return ;
-}
-
-
-std::ostream& operator<<(std::ostream& out, const User& User)
-{
-	std::string input = User.input;
-	std::string userDetails = "UserName: [" + User.userName + "]" + ", Nick: " + "[" + User.nickName + "]" + ", Auth: "
-		+ "[" + (User.isAuth ? "YES" : "NO") + "]" + ", last inputs: " + "[" + input.erase(input.length() - 1, 1) + "]" + ".";
-
-    out << userDetails;
-    return out;
-}
-
-bool	User::parse_cmd(std::string str)
-{
-	std::vector<std::string> vector =  Utils::split(str);
-
-	for(std::vector<std::string>::iterator it = vector.begin(); it != vector.end();)
-	{
-		if(*it == "USER")
-		{
-			if(*it == vector.back())
-				return false;
-			else
-				_cmd[0] = *(++it);
-		}
-		if(*it == "NICK")
-		{
-			if(*it == vector.back())
-				return false;
-			else
-				_cmd[1] = *(++it);
-		}
-		if(*it == "PASS")
-		{
-			if(*it == vector.back())
-				return false;
-			else
-				pass = *(++it);
-		}
-		else
-			++it;
-	}
-
-	return true;
+		
 }
